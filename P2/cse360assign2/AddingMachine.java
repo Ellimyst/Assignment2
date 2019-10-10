88d03e1ee0df7a804aaa57cf9ca7496ccf64f2d2
Sumukh Vaze <sumukhvaze@gmail.com>
2019-10-09 18:52:56 -0700

    Commit for 1-c

---
+++ P2/cse360assign2/AddingMachine.java
@@ -0,0 +1,41 @@
+//Sumukh Vaze
+//CSE 360
+//Assignment 1
+
+package cse360assign2;
+
+//AddingMachine adds or subtracts values to a total which it returns.
+public class AddingMachine {
+
+	private int total;
+	
+	//constructor
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	//get total
+	public int getTotal () {
+		return 0;
+	}
+	
+	//add value to total
+	public void add (int value) {
+		
+	}
+	
+	//subtract value from total
+	public void subtract (int value) {
+		
+	}
+		
+	//return total
+	public String toString () {
+		return "";
+	}
+
+	//reset total to 0
+	public void clear() {
+	
+	}
+}
