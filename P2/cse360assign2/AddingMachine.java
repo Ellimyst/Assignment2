3274b9db8738448349a27d7d0724cc3614197da0
Sumukh Vaze <sumukhvaze@gmail.com>
2019-10-09 20:21:45 -0700

    Commit for 2-b

--- P2/cse360assign2/AddingMachine.java
+++ P2/cse360assign2/AddingMachine.java
@@ -8,34 +8,39 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String history;
 	
 	//constructor
 	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
+		total = 0;  // not needed - included for clarity\
+		history = "0";
 	}
 	
 	//get total
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	//add value to total
 	public void add (int value) {
-		
+		total += value;
+		history = history +" + " +value;
 	}
 	
 	//subtract value from total
 	public void subtract (int value) {
-		
+		total -= value;
+		history = history +" - " +value;
 	}
 		
-	//return total
+	//return history
 	public String toString () {
-		return "";
+		return history;
 	}
 
 	//reset total to 0
 	public void clear() {
-	
+		total = 0;
+		history = "0";
 	}
 }
